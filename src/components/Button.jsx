5e51c32ea29999bf897287ffdee262663403fa49
Aditya Verma <119062848+AdityaVerma10@users.noreply.github.com>
2024-02-27 14:44:36 +0530

    Update Button.jsx

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 
 function Button({
-    width,
-    content
+  
+    content,
+    className=''
 }) {
-  console.log(width)
+
   return (
     
-    <button className={`w-[${width}px] h-[48px]  rounded-[12px] bg-[#1B88F4] text-[16px] text-[#FFFFFF] font-[400]`} >
+    <button className={`${className} h-[48px] rounded-[12px] bg-[#1B88F4] text-[16px] text-[#FFFFFF] font-[400]`} >
                     {content}
     </button>
   )
